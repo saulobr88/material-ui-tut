e9a91bb03de1990e6f7e35330b8a11ab54bb0929
Saulo Gomes <saulo.sgs@ufpe.br>
2022-12-20 12:51:53 -0300

    Typography

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function Notes() {
   return (
     <div>
-      Notes page
+      <Typography variant="h1" gutterBottom>
+        Create New Note
+      </Typography>
+      <Box sx={{ width: '100%', maxWidth: 500 }}>
+        <Typography variant="h2" gutterBottom>
+          h2. Heading
+        </Typography>
+      </Box>
     </div>
   )
 }
